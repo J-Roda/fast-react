f599beb638e10c476f2d3d29ccf227f3a8660b1f
Jayson Roda <jayson.roda@awsys-i.com>
2023-10-13 18:53:56 +0800

    Add some styles with react-tailwind

---
+++ src/features/order/SearchOrder.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function SearchOrder() {
+  const [query, setQuery] = useState();
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!query) return;
+
+    navigate(`/order/${query}`);
+    setQuery("");
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Search order #"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        className="px-4 py-2 w-28 text-sm bg-yellow-50 rounded-full placeholder:text-stone-400 sm:w-64 sm:focus:w-72 transition-all duration-300 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50"
+      />
+    </form>
+  );
+}
+export default SearchOrder;
