321b32597ffbd4f8177a2e13c5e03308072cff8e
Jayson Roda <jayson.roda@awsys-i.com>
2023-10-15 20:59:26 +0800

    Add cart and user redux state

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,10 +1,23 @@
+import { useDispatch } from "react-redux";
 import Button from "../../ui/Button";
 import { formatCurrency } from "../../util/helpers";
+import { addItem } from "../cart/cartSlice";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
-  console.log(ingredients);
+  const dispatch = useDispatch();
+
+  function handleAddToCart() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+    dispatch(addItem(newItem));
+  }
   return (
     <li className="flex gap-4 py-2 pt-0.5">
       <img
@@ -25,7 +38,11 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
-          <Button type="small">Addto cart</Button>
+          {!soldOut && (
+            <Button type="small" onClick={handleAddToCart}>
+              Addto cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
